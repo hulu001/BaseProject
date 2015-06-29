4ae2c5e49b539af8c51b69bb1734e63495f4135b
hulu001 <hulu001@yeah.net>
2015-06-29 18:50:56 +0800

    Initial Commit

---
+++ BaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseProject
+//
+//  Created by zhupetter on 15/6/29.
+//  Copyright (c) 2015年 zhupetter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
